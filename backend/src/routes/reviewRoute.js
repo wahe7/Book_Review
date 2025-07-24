f59b1338b2134c4436cec4985d183f8fb41213ae
wahe7 <waheguru002200@gmail.com>
2025-07-24 19:19:51 +0530

    add reviews routes

--- backend/src/routes/reviewRoute.js
+++ backend/src/routes/reviewRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const {createReview} = require("../controllers/reviewController");
+const {createReview, getReview} = require("../controllers/reviewController");
 const authMiddleware = require("../middlewares/authMiddleware");
 
-router.post("/",authMiddleware,createReview);
+router.post("/:bookId", authMiddleware, createReview);
+router.get("/:bookId",getReview);
 
 module.exports = router;
