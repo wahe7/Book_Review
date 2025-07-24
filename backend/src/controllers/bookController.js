5df392fb6ef6a845e2686637e0e8d6ff61a9f492
wahe7 <waheguru002200@gmail.com>
2025-07-24 22:48:42 +0530

    adding pagination

--- backend/src/controllers/bookController.js
+++ backend/src/controllers/bookController.js
@@ -24,33 +24,29 @@ exports.createBook = async (req, res) => {
 };
 
 exports.getBooks = async (req, res) => {
-  const {genre, author} = req.query;
-  try {
-    const books = await prisma.book.findMany({
-      where:{
-        ...(genre && {
-          genre: {
-            contains: genre,
-            mode: 'insensitive'
-          }
-        }),
-        ...(author && {
-          author: {
-            contains: author,
-            mode: 'insensitive'
-          }
-        })
-      },
-      include:{
-        reviews:true
-      },
-      orderBy:{
-        createdAt:"desc"
-      }
-    });
+  const {genre, author, page=1, limit=5} = req.query;
+  const skip = (page-1)*limit;
+  const filter = {
+    ...(genre && { genre: { contains: genre, mode: 'insensitive' } }),
+    ...(author && { author: { contains: author, mode: 'insensitive' } }),
+  };
+  console.log(filter);
+  try{
+    const [books, totalCount] = await Promise.all([
+      prisma.book.findMany({
+        where:filter,
+        skip: Number(skip),
+        take: Number(limit),
+        include: { reviews: true },
+        orderBy: { createdAt: 'desc' },
+      }),
+      prisma.book.count({ where:filter }),
+    ]);
+    console.log(books);
     if(!books){
       return res.status(404).json({ message: "No books found" });
     }
+    const totalPages = Math.ceil(totalCount / limit);
 
     const allbooks = books.map(book=>{
       const reviews = book.reviews || [];
@@ -60,7 +56,7 @@ exports.getBooks = async (req, res) => {
       return {...book,avgReview:avg, reviewCount:reviews.length};
     })
 
-    res.json({allbooks});
+    res.json({allbooks,totalPages, currentPage: Number(page)});
   } catch (err) {
     return res.status(500).json({ message: "Internal server error" });
   }
@@ -69,11 +65,16 @@ exports.getBooks = async (req, res) => {
 exports.getBookById = async (req, res) => {
   const {id} = req.params;
   try {
-    const book = await prisma.book.findUnique({where:{id:parseInt(id)}});
+    const book = await prisma.book.findUnique({where:{id:parseInt(id)},include:{reviews:true}});
+
     if(!book){
       return res.status(404).json({ message: "Book not found" });
     }
-    return res.status(200).json({ book });
+    const reviews = book.reviews || [];
+    const rating = reviews.map(r=>r.rating).filter(Boolean);
+    const avg = rating.length> 0? rating.reduce((a,b)=>a+b,0)/rating.length:null;
+    const bookRes = {...book,averageRating:avg,reviewCount:reviews.length};
+    return res.status(200).json({ bookRes });
   } catch (err) {
     return res.status(500).json({ message: "Internal server error" });
   }
